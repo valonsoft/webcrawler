856b2493d3ee07f382debbae889fdf168ecb4641
valonsoft <valonsedai@gmail.com>
2016-03-04 06:32:16 +0100

    Update iRobot.php

--- lib/iRobot.php
+++ lib/iRobot.php
@@ -39,47 +39,45 @@ class iRobot
 
             case 'nairaland.com' :
                 // get topic, poster, date and views
-                $td = $xpath -> query("//td[@id]");
+                $td = $valonXpath -> query("//td[@id]");
                 foreach($td as $td) {
-                    $topic = $xpath -> query("b[1]/a[1]", $td) -> item(0) -> textContent;
-                    $topicURL = $xpath -> query("b[1]/a[1]", $td) -> item(0) -> getAttribute('href');
-                    $poster = $xpath -> query("span[1]/b[1]", $td) -> item(0) -> textContent;
-                    $views = $xpath -> query("span[1]/b[3]", $td) -> item(0) -> textContent;
-                    $date = $xpath -> query("span[1]/b[5]", $td) -> item(0) -> textContent;
 
-                    // write the crawled data to file
-                    $line = $new_url.$topicURL . "\t" . $topic . "\t" . $poster . "\t" . $date . "\t" . $views . "\t" . PHP_EOL;
-                    echo $line; file_put_contents('php/crawler_cache/data', $line, FILE_APPEND);
+                    $data = array();
+                    $data[ 'topic']= $valonXpath -> query("b[1]/a[1]", $td) -> item(0) -> textContent;
+                    $data['url'] = $valonXpath -> query("b[1]/a[1]", $td) -> item(0) -> getAttribute('href');
+                    $data['poster'] = $valonXpath -> query("span[1]/b[1]", $td) -> item(0) -> textContent;
+                    $data['views'] = $valonXpath -> query("span[1]/b[3]", $td) -> item(0) -> textContent;
+                    $data['date'] = $valonXpath -> query("span[1]/b[5]", $td) -> item(0) -> textContent;
+
+                    // save to the database
+                    $this->saveData('nairaland', $data);
 
                 }
                 break;
             case 'stackoverflow.com' :
                 // find the questions
-                $questions = $xpath -> query("//div[@class='question-summary']");
+                $questions = $valonXpath -> query("//div[@class='question-summary']");
                 foreach($questions as $question) {
-                    $topic = $xpath -> query("div[2]/h3[1]", $question) -> item(0) -> textContent;
-                    $topicURL = $xpath -> query("div[2]/h3[1]/a[1]", $question) -> item(0) -> getAttribute('href');
-                    $poster = $xpath -> query("div[2]/div[3]/*/div[@class='user-details']/a[last()]", $question) -> item(0) -> textContent;
-                    $upvotes = $xpath -> query("div[1]/div[2]/div[1]/*/span[1]", $question) -> item(0) -> textContent;
-                    $date = $xpath -> query("div[2]/div[3]/*/div[@class='user-action-time']/span[1]", $question) -> item(0) -> textContent;
 
-                    // write crawled data to file
-                    $line = $new_url.$topicURL . "\t" . $topic . "\t" . $poster . "\t" . $date . "\t" . "\t" . $upvotes . PHP_EOL;
-                    echo $line; file_put_contents('php/crawler_cache/data', $line, FILE_APPEND);
+                    $data = array();
+                    $data[ 'topic'] = $valonXpath -> query("div[2]/h3[1]", $question) -> item(0) -> textContent;
+                    $data['url'] = $valonXpath -> query("div[2]/h3[1]/a[1]", $question) -> item(0) -> getAttribute('href');
+                    $data['poster'] = $valonXpath -> query("div[2]/div[3]/*/div[@class='user-details']/a[last()]", $question) -> item(0) -> textContent;
+                    $data['votes'] = $valonXpath -> query("div[1]/div[2]/div[1]/*/span[1]", $question) -> item(0) -> textContent;
+                    $data['date'] = $valonXpath -> query("div[2]/div[3]/*/div[@class='user-action-time']/span[1]", $question) -> item(0) -> textContent;
+
+                    // save to the database
+                    $this->saveData('stackoverflow', $data);
                 }
 
-                // finish url setup
-                $page = $i + 1;
-                $new_url .= '/questions?page=' . $page . '&sort=votes';
 
         }
 
-        // complete url setup
-        $url = $new_url;
+
 
     }
 
-    }
+
 
     /**
      * save the indexed data to the database table
